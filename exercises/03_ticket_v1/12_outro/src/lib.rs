6ec28ae244bbfd04a6e03df3fe7ebd0acf520592
rubengglez <rubengglez@gmail.com>
2024-07-12 20:16:11 +0200

    finish first block

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,72 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: usize,
+    unit_price: usize,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: usize, unit_price: usize) -> Self {
+        let product_name = Order::validate_name(product_name);
+        let quantity = Order::validate_quantity(quantity);
+        let unit_price = Order::validate_unit_price(unit_price);
+
+        Order {
+            product_name,
+            unit_price,
+            quantity,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        self.product_name.as_str()
+    }
+
+    pub fn quantity(&self) -> &usize {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &usize {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, name: String) {
+        self.product_name = Order::validate_name(name);
+    }
+
+    pub fn set_quantity(&mut self, quantity: usize) {
+        self.quantity = Order::validate_quantity(quantity);
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: usize) {
+        self.unit_price = Order::validate_unit_price(unit_price);
+    }
+
+    pub fn total(&self) -> usize {
+        self.quantity * self.unit_price
+    }
+
+    fn validate_name(name: String) -> String {
+        if name.len() == 0 || name.len() > 300 {
+            panic!("con not be empty");
+        }
+        name
+    }
+
+    fn validate_quantity(value: usize) -> usize {
+        if value <= 0 {
+            panic!("can not be 0");
+        }
+        value
+    }
+
+    fn validate_unit_price(value: usize) -> usize {
+        if value <= 0 {
+            panic!("can not be 0");
+        }
+        value
+    }
+}
